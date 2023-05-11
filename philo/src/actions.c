26bf9bcacf311ae2aba819208d0c79ce1e80fd6c
Rodrigo Carvalho <rodrigoadcarvalho@gmail.com>
2023-05-11 12:35:41 -0300

    solved deadlock alternating fork distribution - odd and even philos - Thiago

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -2,32 +2,54 @@
 
 void *routine(void *arg)
 {    
-    t_philo data_philo = *(t_philo *)arg;
+    t_philo ph = *(t_philo *)arg;
     
     while (1) {
         // Philosopher thinks
-        printf("Philosopher %d is thinking...\n", data_philo.index);
+        printf("Philosopher %d is thinking...\n", ph.index);
         
         // Philosopher sleeps
-        printf("Philosopher %d is sleeping...\n", data_philo.index);
-        usleep(data_philo.data->t_sleep);
+        printf("Philosopher %d is sleeping...\n", ph.index);
+        usleep(ph.data->t_sleep);
 
-        // Philosopher tries to grab left fork
-        pthread_mutex_lock(data_philo.left);
-        printf("Philosopher %d took fork %p.\n", data_philo.index, data_philo.left);
-        
-        // Philosopher tries to grab right fork
-        pthread_mutex_lock(data_philo.right);
-        printf("Philosopher %d took fork %p.\n", data_philo.index, data_philo.right);
-        
+        // Odd philosopher try to grab left fork
+        if (ph.index % 2 != 0)
+        {
+            pthread_mutex_lock(ph.left);
+            printf("Philosopher %d took fork %p.\n", ph.index, ph.left);
+        }
+
+        // Even philosopher try to grab right fork
+        if (ph.index % 2 == 0)
+        {
+            pthread_mutex_lock(ph.right);
+            printf("Philosopher %d took fork %p.\n", ph.index, ph.right);
+        }
+
+        // Odd philosopher try to grab right fork
+        if (ph.index % 2 != 0)
+        {
+            pthread_mutex_lock(ph.right);
+            printf("Philosopher %d took fork %p.\n", ph.index, ph.right);
+        }
+
+        // Even philosopher try to grab left fork
+        if (ph.index % 2 == 0)
+        {
+            pthread_mutex_lock(ph.left);
+            printf("Philosopher %d took fork %p.\n", ph.index, ph.left);
+        }
+
         // Philosopher eats
-        printf("Philosopher %d is eating...\n", data_philo.index);
-        usleep(data_philo.data->t_eat);
+        printf("Philosopher %d is eating...\n", ph.index);
+        usleep(ph.data->t_eat);
+        ph.t_last_meal = curr_time() - ph.data->ini_time;
         
         // Philosopher releases forks
-        pthread_mutex_unlock(data_philo.right);
-        printf("Philosopher %d released fork %p.\n", data_philo.index, data_philo.right);
-        pthread_mutex_unlock(data_philo.left);
-        printf("Philosopher %d released fork %p.\n", data_philo.index, data_philo.left);
+        pthread_mutex_unlock(ph.right);
+        printf("Philosopher %d released fork %p.\n", ph.index, ph.right);
+        pthread_mutex_unlock(ph.left);
+        printf("Philosopher %d released fork %p.\n", ph.index, ph.left);
+
     }
 }

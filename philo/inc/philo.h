c3ed75517323eaee361bf53804044151a21e365e
Rodrigo Carvalho <rodrigoadcarvalho@gmail.com>
2023-05-17 15:06:27 -0300

    all working on 42 desktop - all normed

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -1,16 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 14:43:52 by rcarvalh          #+#    #+#             */
+/*   Updated: 2023/05/17 14:51:34 by rcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PHILO_H
 # define PHILO_H
 
 //includes
-# include <pthread.h> // threads
-# include <stdio.h> // printf
-# include <stdlib.h> // malloc array of structs (philos) and forks (mutexes)
-# include <sys/time.h> // gettimeofday
-# include <unistd.h> // write
-
-typedef struct s_data_philo	t_data_philo;
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
 //parse information - public info
+typedef struct s_data_philo	t_data_philo;
+
 typedef struct s_info
 {
 	unsigned long	ini_time;
@@ -20,10 +32,10 @@ typedef struct s_info
 	unsigned long	t_sleep;
 	int				n_meals;
 	int				death;
-	pthread_mutex_t *mutex;
+	pthread_mutex_t	*mutex;
 	pthread_mutex_t	death_mutex;
 	pthread_mutex_t	last_meal_mutex;
-	pthread_mutex_t n_meals_mutex;
+	pthread_mutex_t	n_meals_mutex;
 	t_data_philo	*ph;
 }	t_info;
 
@@ -49,29 +61,28 @@ int				check_empty(int argc, char **argv);
 int				check_char(int argc, char **argv);
 int				check_max_int(int argc, char **argv);
 int				validation(int argc, char **argv);
-void			print_args(int argc, char **argv);
 
 //function prototypes - time
-unsigned long	curr_time(t_data_philo *ph);
 unsigned long	prog_start(void);
-void	smart_sleep(unsigned int time, t_data_philo *ph);
+unsigned long	curr_time(t_data_philo *ph);
+void			smart_sleep(unsigned int time, t_data_philo *ph);
 
-//function prototypes - philosophers
+//function prototypes - init
+t_info			*parse_pub_info(int argc, char **argv);
 t_data_philo	*priv_data_philos(t_info *info);
 void			create_th_philos(t_info *info, t_data_philo *ph);
 void			join_threads(t_data_philo *ph);
-
-//function prototypes - forks
 pthread_mutex_t	*create_forks(t_info *info);
-pthread_mutex_t	*create_death_mutex(t_data_philo *ph);
 
-//function prototypes - data
-t_info			*parse_pub_info(int argc, char **argv);
+//function prototypes - monitor
+int				check_nbr_meals(t_data_philo *ph);
+void			*death_monitor(t_data_philo *ph);
 
 //function prototypes - routine
-void			*routine(void *arg);
+void			take_forks(t_data_philo *ph);
 void			ph_eat(t_data_philo *ph);
-void			*death_monitor(t_data_philo *ph);
+void			actions(t_data_philo *ph);
+void			*routine(void *arg);
 void			print_action(t_data_philo *ph, char *action);
 
 #endif

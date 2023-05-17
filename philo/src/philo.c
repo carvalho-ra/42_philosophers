b29e27f0e863497389d879c49daa6d008cac299f
Rodrigo Carvalho <rodrigoadcarvalho@gmail.com>
2023-05-17 13:21:38 -0300

    philo.c - norminette ok

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -1,14 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 13:20:10 by rcarvalh          #+#    #+#             */
+/*   Updated: 2023/05/17 13:20:52 by rcarvalh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/philo.h"
 
 int	main(int argc, char **argv)
 {
-	t_info	*info;
-	t_data_philo *ph;
+	t_info			*info;
+	t_data_philo	*ph;
 
 	if (validation(argc, argv) == 1)
 		return (0);
-	//fix mutex initialization
-	//mutex = create_forks(info);
 	info = parse_pub_info(argc, argv);
 	ph = priv_data_philos(info);
 	create_th_philos(info, ph);
@@ -16,9 +26,9 @@ int	main(int argc, char **argv)
 	join_threads(ph);
 	pthread_mutex_destroy(&info->death_mutex);
 	pthread_mutex_destroy(&info->last_meal_mutex);
-	pthread_mutex_destroy(info->mutex);//fix mutex initialization
+	pthread_mutex_destroy(info->mutex);
 	free(ph);
-	free(info->mutex);//fix mutex initialization
+	free(info->mutex);
 	free(info);
 	return (0);
 }

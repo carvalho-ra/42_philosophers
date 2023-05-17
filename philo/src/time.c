e6411d2ee32ff240ea95457dd792d772126278f7
Rodrigo Carvalho <rodrigoadcarvalho@gmail.com>
2023-05-17 14:29:51 -0300

    time.c - norminette ok

--- philo/src/time.c
+++ philo/src/time.c
@@ -6,7 +6,7 @@
 /*   By: rcarvalh <rcarvalh@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:44:59 by rcarvalh          #+#    #+#             */
-/*   Updated: 2023/05/17 11:45:00 by rcarvalh         ###   ########.fr       */
+/*   Updated: 2023/05/17 14:29:10 by rcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 unsigned long	prog_start(void)
 {
-	struct timeval tm;
+	struct timeval	tm;
+
 	gettimeofday(&tm, NULL);
 	return ((tm.tv_sec * 1000) + (tm.tv_usec / 1000));
 }
 
 unsigned long	curr_time(t_data_philo *ph)
 {
+	struct timeval	tm;
 
-	struct timeval tm;
 	gettimeofday(&tm, NULL);
 	return (((tm.tv_sec * 1000) + (tm.tv_usec / 1000)) - ph->info->ini_time);
 }
@@ -31,9 +32,8 @@ void	smart_sleep(unsigned int time, t_data_philo *ph)
 {
 	unsigned long	tm1;
 	unsigned long	tm2;
-
-	struct timeval first;
-	struct timeval second;
+	struct timeval	first;
+	struct timeval	second;
 
 	gettimeofday(&first, NULL);
 	tm1 = (first.tv_sec * 1000) + (first.tv_usec / 1000);
@@ -49,7 +49,7 @@ void	smart_sleep(unsigned int time, t_data_philo *ph)
 		gettimeofday(&second, NULL);
 		tm2 = (second.tv_sec * 1000) + (second.tv_usec / 1000);
 		if (tm2 - tm1 >= time)
-			break;
+			break ;
 		usleep(100);
 	}
 }
